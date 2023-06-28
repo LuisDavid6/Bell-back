f8fa0f2558ac88b84aa223035bbc4d8f4d290939
David <luisdavid.0698@hotmail.com>
2023-06-28 13:20:41 -0500

    fix: change cors to dotenv config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { config } from 'dotenv'
+
+config()
 
 const PORT = process.env.PORT
 
