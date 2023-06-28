6c4a7317cc2c9645161a49585f7f6454337c5279
David <luisdavid.0698@hotmail.com>
2023-06-28 16:00:24 -0500

    fix: add headers to vercel.json

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,8 @@ const PORT = process.env.PORT
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authorization',
-    ],
-    credentials: true,
-  })
+  app.enableCors()
+
   //validations
   app.useGlobalPipes(new ValidationPipe())
   //configurar títulos de documentación
